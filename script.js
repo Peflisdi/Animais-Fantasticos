f0886ceb7398628f82c90fceb4a2a464456d89da
Pedro Henrique <peflisdi@gmail.com>
2023-12-17 13:24:07 -0300

    finalizando o html e css do site

---
+++ script.js
@@ -0,0 +1,24 @@
+// // Retorne no console todas as imagens do site
+// const allImages = document.querySelectorAll('img')
+
+// console.log(allImages)
+
+// // Retorne no console apenas as imagens que começaram com a palavra imagem
+// const imagensAnimais = document.querySelectorAll('img[src^="img/imagem')
+
+// console.log(imagensAnimais)
+
+// // Selecione todos os links internos (onde o href começa com #)
+// const linksInternos = document.querySelectorAll('a[href^="#"]')
+
+// console.log(linksInternos)
+
+// // Selecione o primeiro h2 dentro de .animais-descricao
+// const primeiroH2 = document.querySelector('.animais-descricao h2')
+
+// console.log(primeiroH2.innerText)
+
+// // Selecione o último p do site
+// const ultimoP = document.querySelectorAll('p')
+
+// console.log(ultimoP[ultimoP.length - 1])
